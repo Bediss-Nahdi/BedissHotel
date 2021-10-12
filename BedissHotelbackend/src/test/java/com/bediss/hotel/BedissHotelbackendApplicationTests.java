d742fb57a5bc2e18b4e6e3d50d224388cf927be6
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-10-12 18:07:37 +0200

    initial commit

---
+++ BedissHotelbackend/src/test/java/com/bediss/hotel/BedissHotelbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bediss.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedissHotelbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
